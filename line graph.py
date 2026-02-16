871970073e5736a71c78fa4b8595ceb8a98907f7
Husnain <Husnain.tayab@hotmail.com>
2026-02-16 20:47:26 +0500

    Line graph

--- line graph.py
+++ line graph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 6, 8, 10]
-z = [11,22,33,44,55]
+x = [1, 2, 3, 4, 5,6]
+y = [2, 4, 6, 8, 10,12]
+z = [11,22,33,44,55,66]
 
 plt.plot(x, y,color="r",label=" Line Graph")
 plt.plot(x, z,color="b",label=" Line Graph")
