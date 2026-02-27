d87f58b6b6c32a17a045f665377e7f2f3c0f220d
Husnain <Husnain.tayab@hotmail.com>
2026-02-27 23:19:18 +0500

    Bar graph

--- bar graph 2.py
+++ bar graph 2.py
@@ -4,8 +4,10 @@ import numpy as np
 language = ["python","c","c++","java","react","ruby"]
 demand = [80,70,75,78,64,54]
 demand2 = [60,50,45,38,24,14]
+
 col = ["y","g","r","b","black"]
 width = 0.2
+
 p = np.arange(len(language))
 p1 = [j+width for j in p]
 
