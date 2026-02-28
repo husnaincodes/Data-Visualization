1a441a5ba84d8e434960424372165b05af8fa455
Husnain <Husnain.tayab@hotmail.com>
2026-02-28 23:12:31 +0500

    Star Pattern

--- line2 graph.py
+++ line2 graph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoint= np.array([50,6,50, 80, 70])
-ypoint=np.array([100,250, 80, 90, 50])
+xpoint= np.array([50,6,50, 80, 70,100])
+ypoint=np.array([100,250, 80, 90, 50,30])
 
 plt.plot(xpoint,ypoint, '*')
 plt.show()
