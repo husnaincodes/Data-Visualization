2f83fdd2f4ae76c7a8cc9e5fb3f36ce0d956c98b
Husnain <Husnain.tayab@hotmail.com>
2026-04-07 21:57:37 +0500

    Stars graph

--- line2 graph.py
+++ line2 graph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoint= np.array([50,6,50, 80, 70,100])
-ypoint=np.array([100,250, 80, 90, 50,30])
+xpoint= np.array([50,6,50, 80, 70,100,200])
+ypoint=np.array([100,250, 80, 90, 50,30,20])
 
 plt.plot(xpoint,ypoint, '*')
 plt.show()
