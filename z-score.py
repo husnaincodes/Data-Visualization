351bb494502e7107d4499e22bcd7815277d012b7
Husnain <Husnain.tayab@hotmail.com>
2026-04-24 22:43:32 +0500

    Z-score

--- z-score.py
+++ z-score.py
@@ -1,22 +1,21 @@
 import math
 from scipy.stats import norm
 
-# Given values
+
 x_bar = 10.455
 sigma = 7.7
 n = 44
 confidence = 0.90
 
-# Z critical value
 z = norm.ppf(1 - (1 - confidence) / 2)
 
-# Standard error
+
 SE = sigma / math.sqrt(n)
 
-# Margin of error
+
 ME = z * SE
 
-# Confidence interval
+
 lower = x_bar - ME
 upper = x_bar + ME
 
