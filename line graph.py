23709515d6272d96e14ff17b4363006bd4f75904
Husnain <Husnain.tayab@hotmail.com>
2026-04-30 23:06:30 +0500

    line graph

--- line graph.py
+++ line graph.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5,6]
 y = [2, 4, 6, 8, 10,12]
 z = [11,22,33,44,55,66]
+a = [11,22,33,44,55,66]
 
 plt.plot(x, y,color="r",label=" Line Graph")
 plt.plot(x, z,color="b",label=" Line Graph")
+plt.plot(x, a,color="g",label=" Line Graph")
 plt.xlabel("X values")
 plt.ylabel("Y values")
 plt.title("My First Plot")
